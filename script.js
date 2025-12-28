84f086a09bfad554d2c744f78344210d3c0d3d50
Akash Desani <Desanitechnology@gmail.com>
2025-12-28 15:08:52 +0530

    Change API URL for contact form
    Updated API URL for contact form submission.

--- script.js
+++ script.js
@@ -1,22 +1,4 @@
-const API_URL = "https://future-fs-01-q3sv.onrender.com";
-fetch("https://future-fs-01-q3sv.onrender.com/contact", {
-  method: "POST",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify({
-    name: nameValue,
-    email: emailValue,
-    message: messageValue
-  })
-})
-.then(res => res.json())
-.then(data => {
-  alert(data.message);
-})
-.catch(err => {
-  alert("Server error!");
-});
+fetch("https://portfolio-backend-07nz.onrender.com/contact")
 
 
 (function () {
